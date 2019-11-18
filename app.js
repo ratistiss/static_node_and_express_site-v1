689ff8e9bcb82b931318866cee6b6d5288524711
ratistiss <50683985+ratistiss@users.noreply.github.com>
2019-11-18 17:44:02 -0500

    update 1.3

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 
 const express = require('express');
-const data = require('./data.json');
-
 const app = express();
-app.get('/', (req,res) =>{
-    res.send('were connected');
+const data = require('./data.json');
+const {projects} = data;
 
+app.set('view engine', 'pug');
+app.use('/static/', express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.render('index');
 })
 
+app.get('/about', (req, res) => {
+    res.render('about');
+})
 
-
-
+// app.get("/projects/:id",  (req, res) => {
+//     res.render("project.id", {
+       
+//     });
+// });
 
 
 
