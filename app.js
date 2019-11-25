6d0941af8e04377dcc713923d36bbe2acf8afdbf
ratistiss <50683985+ratistiss@users.noreply.github.com>
2019-11-25 10:18:08 -0500

    Update 2.7
    Modular Routes and Color changes finished

--- app.js
+++ app.js
@@ -8,48 +8,21 @@ app.set('view engine', 'pug');
 app.use('/static/', express.static('public'));
 app.use('/static/', express.static('images'));
 
-const routes = require('./routes');
-
-app.use(routes);
-
-// app.get('/', (req, res) => {
-//     res.render('index', { projects });
-// })
-
-// app.get('/about', (req, res) => {
-//     res.render('about');
-// })
-
-// app.get("/projects/:id",  (req, res,next) => {
-//     const projectID = req.params.id;
-//     const project = projects.find(({id}) => id === +projectID);
-
-//     if(project){
-//      res.render("project", { project })
-//      } else {
-//         // res.sendStatus(404);
-//         const err = new Error('Page Not Found');
-//         err.status = 404;
-//         next(err);
-//      }
-       
-//   });
+const mainRoutes = require('./routes');
+const projectsRoutes = require('./routes/projects');
+app.use(mainRoutes);
+app.use(projectsRoutes);
 
 app.use((req, res, next) => {
     const err = new Error('Page Not Found');
     err.status = 404;
     next(err);
-
-
 });
-
 
 app.use((err, req, res, next) =>{
     res.locals.error = err;
     res.status(err.status);
     res.render('error');
-
-
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 
 const express = require('express');
 const router = express.Router();
+const data = require('../data.json');
+const { projects } = data;
 
 router.get('/', (req, res) => {
     res.render('index', { projects });
@@ -10,19 +12,5 @@ router.get('/about', (req, res) => {
     res.render('about');
 })
 
-router.get("/projects/:id", (req, res, next) => {
-    const projectID = req.params.id;
-    const project = projects.find(({ id }) => id === +projectID);
-
-    if (project) {
-        res.render("project", { project })
-    } else {
-        // res.sendStatus(404);
-        const err = new Error('Page Not Found');
-        err.status = 404;
-        next(err);
-    }
-
-});
 
 module.exports = router;
